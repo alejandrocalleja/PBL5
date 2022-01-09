c1b992dfe2e437a4a86ab3132313371f7297c0cd
Izan24 <izan.arcos@alumni.mondragon.edu>
2022-01-09 02:54:22 +0100

    Templates added

---
+++ src/main/java/eus/blankcard/decklearn/controller/ProfileController.java
@@ -0,0 +1,14 @@
+package eus.blankcard.decklearn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+    
+
+    @GetMapping("")
+    public void getProfile() {
+        
+    }
+}
