e6dfcc792f32e6f7a8e493ec4bcbe603d7c31714
Alejandro Calleja <alejandro.calleja@alumni.mondragon.edu>
2022-01-13 09:23:46 +0100

    security hotspots fixed

--- src/main/java/eus/blankcard/decklearn/repository/follow/FollowRepositoryImpl.java
+++ src/main/java/eus/blankcard/decklearn/repository/follow/FollowRepositoryImpl.java
@@ -6,7 +6,6 @@ import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 
-import eus.blankcard.decklearn.models.FollowRelation;
 
 @Repository
 public class FollowRepositoryImpl implements FollowRepositoryCustom {
@@ -24,8 +23,8 @@ public class FollowRepositoryImpl implements FollowRepositoryCustom {
         try {
             result = ((Number) query.getSingleResult()).intValue();
         } catch (Exception e) {
-            System.out.println("Failed to load followers from user " + user_id);
-            e.printStackTrace();
+            // System.out.println("Failed to load followers from user " + user_id);
+            // e.printStackTrace();
         }
 
         return result;
@@ -41,8 +40,8 @@ public class FollowRepositoryImpl implements FollowRepositoryCustom {
         try {
             result = ((Number) query.getSingleResult()).intValue();
         } catch (Exception e) {
-            System.out.println("Failed to load following from user " + user_id);
-            e.printStackTrace();
+            // System.out.println("Failed to load following from user " + user_id);
+            // e.printStackTrace();
         }
 
         return result;
