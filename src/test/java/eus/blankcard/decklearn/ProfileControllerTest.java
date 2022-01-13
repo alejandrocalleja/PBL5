d7e43ba41ef1fba380ab44ff036c2868d56006ce
Alejandro Calleja <alejandro.calleja@alumni.mondragon.edu>
2022-01-13 19:12:58 +0100

    new tests aproximation

--- src/test/java/eus/blankcard/decklearn/ProfileControllerTest.java
+++ src/test/java/eus/blankcard/decklearn/ProfileControllerTest.java
@@ -1,24 +1,15 @@
 package eus.blankcard.decklearn;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.context.WebApplicationContext;
-
-import eus.blankcard.decklearn.controller.ProfileController;
 
 // @ExtendWith(SpringExtension.class)
 // @SpringBootTest
@@ -37,19 +28,19 @@ public class ProfileControllerTest {
   }
 
   @Test
-  @WithMockUser(username= "testUser", roles = "USER")
+  @WithMockUser(username = "testUser", roles = "USER")
   public void shouldReturnUserProfile() throws Exception {
     String url = "/EmeraldOfMurmer";
-    
+
     mockMvc.perform(MockMvcRequestBuilders.get(url))
         .andExpect(MockMvcResultMatchers.status().isOk());
   }
 
   @Test
-  @WithMockUser(username= "testUser", roles = "USER")
+  @WithMockUser(username = "testUser", roles = "USER")
   public void shouldReturnUserProfileNotFound() throws Exception {
     String url = "/notFoundUser";
-    
+
     mockMvc.perform(MockMvcRequestBuilders.get(url))
         .andExpect(MockMvcResultMatchers.status().isNotFound());
   }
@@ -57,8 +48,35 @@ public class ProfileControllerTest {
   @Test
   public void shouldReturnRedirectionError() throws Exception {
     String url = "/notFoundUser";
-    
+
     mockMvc.perform(MockMvcRequestBuilders.get(url))
         .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
   }
+
+  // @Test
+  // @WithMockUser(username = "testUser", roles = "USER")
+  // public void shouldReturnFollowers() throws Exception {
+  //   String url = "/EmeraldOfMurmer/followers";
+
+  //   mockMvc.perform(MockMvcRequestBuilders.get(url))
+  //       .andExpect(MockMvcResultMatchers.status().isOk());
+  // }
+
+  // @Test
+  // @WithMockUser(username = "testUser", roles = "USER")
+  // public void shouldReturnFollowing() throws Exception {
+  //   String url = "/EmeraldOfMurmer/following";
+
+  //   mockMvc.perform(MockMvcRequestBuilders.get(url))
+  //       .andExpect(MockMvcResultMatchers.status().isOk());
+  // }
+
+  // @Test
+  // @WithMockUser(username = "testUser", roles = "USER")
+  // public void shouldReturnFollow() throws Exception {
+  //   String url = "/EmeraldOfMurmer/follow";
+
+  //   mockMvc.perform(MockMvcRequestBuilders.post(url))
+  //       .andExpect(MockMvcResultMatchers.status().isOk());
+  // }
 }
