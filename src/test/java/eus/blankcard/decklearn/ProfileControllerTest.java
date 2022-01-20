bf77353fb1c8b6cf687635d3601677b9b94947f9
Alejandro Calleja <alejandro.calleja@alumni.mondragon.edu>
2022-01-20 20:54:50 +0100

    profile controller test optimized

--- src/test/java/eus/blankcard/decklearn/ProfileControllerTest.java
+++ src/test/java/eus/blankcard/decklearn/ProfileControllerTest.java
@@ -3,6 +3,8 @@ package eus.blankcard.decklearn;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,37 +25,21 @@ class ProfileControllerTest {
     assertNotNull(mockMvc);
   }
 
-  @Test
+  @ParameterizedTest
+  @CsvSource(value = {
+      "/EmeraldOfMurmer, 200",
+      "/testUser, 200",
+      "/notFoundUser, 404"
+  }, nullValues = { "null" })
   @WithMockUser(username = "testUser", roles = "USER")
-  void shouldReturnUserProfile() throws Exception {
+  void shouldTestDifferentProfiles(String url, int status) throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get(url))
+        .andExpect(MockMvcResultMatchers.status().is(status));
+  }
+
+  @Test
+  void shouldNotReturnUserProfile() throws Exception {
     String url = "/EmeraldOfMurmer";
-
-    mockMvc.perform(MockMvcRequestBuilders.get(url))
-        .andExpect(MockMvcResultMatchers.status().isOk());
-  }
-
-  @Test
-  @WithMockUser(username = "testUser", roles = "USER")
-  void shouldReturnOwnUserProfile() throws Exception {
-    String url = "/testUser";
-
-    mockMvc.perform(MockMvcRequestBuilders.get(url))
-        .andExpect(MockMvcResultMatchers.status().isOk());
-  }
-
-  @Test
-  @WithMockUser(username = "testUser", roles = "USER")
-  void shouldReturnUserProfileNotFound() throws Exception {
-    String url = "/notFoundUser";
-
-    mockMvc.perform(MockMvcRequestBuilders.get(url))
-        .andExpect(MockMvcResultMatchers.status().isNotFound());
-  }
-
-  @Test
-  void shouldReturnRedirectionError() throws Exception {
-    String url = "/notFoundUser";
-
     mockMvc.perform(MockMvcRequestBuilders.get(url))
         .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
   }
