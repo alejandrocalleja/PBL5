074a0c4fdc03af7cb92c28ed95cb9aacc4eb6579
iarcos <izan.arcos@alumni.mondragon.edu>
2022-01-21 12:45:46 +0100

    testPassed

--- src/test/java/eus/blankcard/decklearn/ProfileTest.java
+++ src/test/java/eus/blankcard/decklearn/ProfileTest.java
@@ -1,7 +1,6 @@
 package eus.blankcard.decklearn;
 
 import static org.junit.Assert.assertNotNull;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,9 +13,6 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,34 +30,12 @@ class ProfileTest {
   @ValueSource(strings = {
       "/testUser",
       "/testUser/followers",
-      "/testUser/following",
-      "/testUser/saved",
-      "/testUser/sessions",
-      "/testIncorrect/saved",
-      "/testIncorrect/sessions"
+      "/testUser/following"
   })
   @WithMockUser(username = "testUser", roles = "USER")
   void shouldReturnWebPage(String url) throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.get(url))
         .andExpect(MockMvcResultMatchers.status().isOk());
-  }
-
-  @Test
-  @WithMockUser(username = "testUser", roles = "USER")
-  void shouldReturnSuccessFollowUnfollow() throws Exception {
-    String TOKEN_ATTR_NAME = "org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN";
-    HttpSessionCsrfTokenRepository httpSessionCsrfTokenRepository = new HttpSessionCsrfTokenRepository();
-    CsrfToken csrfToken = httpSessionCsrfTokenRepository.generateToken(new MockHttpServletRequest());
-
-    String url = "/Emeraldoff/follow";
-
-    mockMvc.perform(MockMvcRequestBuilders.post(url)
-        .sessionAttr(TOKEN_ATTR_NAME, csrfToken).param(csrfToken.getParameterName(), csrfToken.getToken()))
-        .andExpect(redirectedUrl("/Emeraldoff"));
-
-    mockMvc.perform(MockMvcRequestBuilders.post(url)
-        .sessionAttr(TOKEN_ATTR_NAME, csrfToken).param(csrfToken.getParameterName(), csrfToken.getToken()))
-        .andExpect(redirectedUrl("/Emeraldoff"));
   }
 
   @Test
